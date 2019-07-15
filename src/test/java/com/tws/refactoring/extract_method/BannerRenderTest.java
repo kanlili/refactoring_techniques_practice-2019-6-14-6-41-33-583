aafcb9475be0689dc139412270f793908007e7dc
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-15 18:57:31 +0800

    Refator:change for BannerRender class and BannerRenderTest

--- src/test/java/com/tws/refactoring/extract_method/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_method/BannerRenderTest.java
@@ -1,22 +1,26 @@
 package com.tws.refactoring.extract_method;
 
 import com.tws.refactoring.extract_variable.BannerRender;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class BannerRenderTest {
-      @Test
-      public void should_return_IEonMac_when_call_renderBanner_given_macie(){
-          BannerRender bannerRender=new BannerRender();
+     private static BannerRender bannerRender;
+    @Before
+     public  void init(){
+         bannerRender=new BannerRender();
+     }
+     @Test
+     public void should_return_IEonMac_when_call_renderBanner_given_macie(){
           String result= bannerRender.renderBanner("MAC","IE");
           String expectResult="IE on Mac?";
           assertEquals(expectResult,result);
       }
     @Test
     public void should_return_IEonMac_when_call_renderBanner_given_kacki(){
-        BannerRender bannerRender=new BannerRender();
         String result= bannerRender.renderBanner("KAC","KE");
         String expectResult="banner";
         assertEquals(expectResult,result);
