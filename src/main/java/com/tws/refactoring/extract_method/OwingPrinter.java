a98ce7a479d2726613048a4af8ba7859c9fb1231
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-15 18:23:00 +0800

    Refator:add method for OwingPrinter class

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -6,24 +6,24 @@ import java.util.List;
 import java.util.Vector;
 
 public class OwingPrinter {
+
     void printOwing(String name, List<Order> orders) {
         Iterator<Order> elements = orders.iterator();
         double outstanding = 0.0;
-
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
-
+        printBanner();
         // print owings
         while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
-
         // print details
         System.out.println("name: " + name);
         System.out.println("amount: " + outstanding);
+    }
+    public  void printBanner(){
+        System.out.println ("*****************************");
+        System.out.println ("****** Customer totals ******");
+        System.out.println ("*****************************");
     }
 }
 
